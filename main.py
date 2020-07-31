d1316a9bc50aca870eedb19cb1eb2f3b0ea9d83a
Abhay3008 <54666019+Abhay3008@users.noreply.github.com>
2020-07-31 15:16:01 +0530

    Main program to run

---
+++ main.py
@@ -0,0 +1,131 @@
+# importing all libraries required.
+
+import numpy as np
+import pandas as pd
+from PIL import Image
+import matplotlib.pyplot as plt
+import os
+from keras import preprocessing
+from keras.models import Sequential
+from keras.layers import Conv2D,Dropout,Dense,Flatten,Conv2DTranspose,BatchNormalization,LeakyReLU,Reshape
+import tensorflow as tf
+
+# Creating a list to store all images.
+path_celeb = []
+# set train_path to your path to directory of all images.
+train_path_celeb = "/content/train/africa_fabric/africa_fabric/"
+for path in os.listdir(train_path_celeb):
+    if '.jpg' in path:
+        path_celeb.append(os.path.join(train_path_celeb, path))
+        
+new_path=path_celeb[0:2000]
+
+# Cropping all images to set them of one size
+
+crop = (0 , 0, 64, 64) 
+images = [np.array((Image.open(path).crop(crop)).resize((64,64))) for path in new_path]
+
+for i in range(len(images)):
+    images[i] = ((images[i] - images[i].min())/(255 - images[i].min()))
+ 
+images = np.array(images) 
+train_data=images
+X_train = train_data
+noise_shape = 100
+
+# Creating Generator model which will generate new images for us.
+generator=Sequential()
+generator.add(Dense(4*4*512,input_shape=[noise_shape]))
+generator.add(Reshape([4,4,512]))
+generator.add(Conv2DTranspose(256, kernel_size=4, strides=2, padding="same"))
+generator.add(LeakyReLU(alpha=0.2))
+generator.add(BatchNormalization())
+generator.add(Conv2DTranspose(128, kernel_size=4, strides=2, padding="same"))
+generator.add(LeakyReLU(alpha=0.2))
+generator.add(BatchNormalization())
+generator.add(Conv2DTranspose(128, kernel_size=4, strides=2, padding="same"))
+generator.add(LeakyReLU(alpha=0.2))
+generator.add(BatchNormalization())
+generator.add(Conv2DTranspose(3, kernel_size=4, strides=2, padding="same",
+                                 activation='sigmoid'))
+                                 
+# Creating discriminator model which will discriminate fake images with real images.
+
+discriminator=Sequential()
+discriminator.add(Conv2D(32, kernel_size=4, strides=2, padding="same",input_shape=[64,64,3]))
+discriminator.add(Conv2D(64, kernel_size=4, strides=2, padding="same"))
+discriminator.add(LeakyReLU(0.2))
+discriminator.add(BatchNormalization())
+discriminator.add(Conv2D(128, kernel_size=4, strides=2, padding="same"))
+discriminator.add(LeakyReLU(0.2))
+discriminator.add(BatchNormalization())
+discriminator.add(Conv2D(256, kernel_size=4, strides=2, padding="same"))
+discriminator.add(LeakyReLU(0.2))
+discriminator.add(Flatten())
+discriminator.add(Dropout(0.5))
+discriminator.add(Dense(1,activation='sigmoid'))
+
+
+GAN =Sequential([generator,discriminator])
+
+discriminator.compile(optimizer='adam',loss='binary_crossentropy')
+discriminator.trainable = False
+
+GAN.compile(optimizer='adam',loss='binary_crossentropy')
+epochs = 10000  #set epoch according to your training dataset.
+batch_size = 128
+
+D_loss=[] 
+G_loss=[]
+
+### with tf.device('/gpu:0')
+for epoch in range(epochs):
+    print(f"Currently on Epoch {epoch+1}")
+    
+    # For every batch in the dataset
+    for i in range(X_train.shape[0]//batch_size):
+        
+            
+        noise=np.random.uniform(-1,1,size=[batch_size,noise_shape])
+        
+        gen_image = generator.predict_on_batch(noise)
+        
+        train_dataset = X_train[i*batch_size:(i+1)*batch_size]
+        #train on real image
+        train_label=np.ones(shape=(batch_size,1))
+        discriminator.trainable = True
+        d_loss1 = discriminator.train_on_batch(train_dataset,train_label)
+        
+        #train on fake image
+        train_label=np.zeros(shape=(batch_size,1))
+        d_loss2 = discriminator.train_on_batch(gen_image,train_label)
+        
+        
+        noise=np.random.uniform(-1,1,size=[batch_size,noise_shape])
+        train_label=np.ones(shape=(batch_size,1))
+        discriminator.trainable = False
+        #train the generator
+        g_loss = GAN.train_on_batch(noise, train_label)
+        D_loss.append(d_loss1+d_loss2)
+        G_loss.append(g_loss)
+        
+         
+    if epoch % 100 == 0:
+        samples = 10
+        x_fake = generator.predict(np.random.normal(loc=0, scale=1, size=(samples,100)))
+
+        for k in range(samples):
+            plt.subplot(2, 5, k+1)
+            plt.imshow(x_fake[k].reshape(64,64,3))
+            plt.xticks([])
+            plt.yticks([])
+
+        
+        plt.tight_layout()
+        plt.show()
+    print('Epoch: %d,  Loss: D_real = %.3f, D_fake = %.3f,  G = %.3f' %   (epoch+1, d_loss1, d_loss2, g_loss))        
+print('Training is complete')
+
+discriminator.save('./discriminator')
+generator.save('./generator')
+GAN.save('./GAN')
